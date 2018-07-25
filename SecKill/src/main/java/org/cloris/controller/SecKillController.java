4c73ba99262cfea6fc48701fbfad3983a0a3ee7e
cloris <clorisforcoding@gmail.com>
2018-07-25 20:49:08 +0800

    initial files

--- SecKill/src/main/java/org/cloris/controller/SecKillController.java
+++ SecKill/src/main/java/org/cloris/controller/SecKillController.java
@@ -1,15 +1,26 @@
 package org.cloris.controller;
 
+import org.cloris.domain.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SecKillController {
 
 	@PostMapping("/secKill")
-	public String secKill() {
-
-		return null;
+	@ResponseBody
+	public String secKill(Model model, User user, @RequestParam("goodsId") Long id) {
+		if(user == null) {
+			return "login";
+		}
+		
+		
+		System.out.println(user);
+		System.out.println(id);
+		return "hahaha";
 	}
 
 }
