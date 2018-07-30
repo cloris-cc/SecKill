9f1cbf594ab1fb6d667f9985f255743169ca29b0
cloris <clorisforcoding@gmail.com>
2018-07-30 22:55:52 +0800

    initial files

--- SecKill/src/main/java/org/cloris/controller/SecKillController.java
+++ SecKill/src/main/java/org/cloris/controller/SecKillController.java
@@ -8,11 +8,13 @@ import org.cloris.service.OrderService;
 import org.cloris.service.SecKillService;
 import org.cloris.vo.CodeMessage;
 import org.cloris.vo.GoodsVO;
+import org.cloris.vo.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SecKillController {
@@ -34,29 +36,25 @@ public class SecKillController {
 	 * 聚合报告：Throughput-1850
 	 */
 	@PostMapping("/secKill")
-	public String secKill(Model model, User user, @RequestParam("goodsId") Long id) {
+	@ResponseBody
+	public Result<OrderInfo> secKill(Model model, User user, @RequestParam("goodsId") Long id) {
 		if (user == null) {
-			return "login";
+			return Result.error(CodeMessage.SESSION_ERROR);
 		}
 		// 判断秒杀商品库存
 		GoodsVO goods = goodsService.findById(id);
 		Integer stock = goods.getSecStock();
 		if (stock <= 0) {
-			model.addAttribute("errmsg", CodeMessage.ACTIVITY_END.getMessage());
-			return "secKill_fail";
+			return Result.error(CodeMessage.ACTIVITY_END);
 		}
 		// 判断是否已经秒杀
 		SecOrder secOrder = orderService.findByUserIdAndGoodsId(user.getId(), id);
 		if (secOrder != null) {
-			model.addAttribute("errmsg", CodeMessage.REPEAT_OPERATION.getMessage());
-			return "secKill_fail";
+			return Result.error(CodeMessage.REPEAT_OPERATION);
 		}
 		// 减库存 下订单 写入秒杀订单
 		OrderInfo order = secKillService.doSecKill(user, goods);
-		model.addAttribute("orderInfo", order);
-		model.addAttribute("goods", goods);
-
-		return "order_detail";
+		return Result.success(order);
 	}
 
 }
---
+++ SecKill/src/main/java/org/cloris/vo/OrderController.java
@@ -0,0 +1,10 @@
+package org.cloris.vo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+	
+	
+	
+}
